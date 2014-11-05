74415a6d6da00867c78b1fc3614e1735f5f5f360
Jromer94 <Jromer94@gmail.com>
2014-11-04 23:48:04 -0500

    added pymongo and setup basic database

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, json
 import requests
 import simplejson
+import pymongo
+
+MONGODB_URI = 'mongodb://jromer:rucrime11@ds051160.mongolab.com:51160/rucrime'
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    client = pymongo.MongoClient(MONGODB_URI)
+    db = client.rucrime
+    collection = db.Crimes
+    docs = collection.find()
+    crimes = docs[0]['crimes']
+    print crimes
     return render_template("map.html")
 
 if __name__ == "__main__":
