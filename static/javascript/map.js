95c96c452bd3e4901c4e7684c39ab5d1ab868695
Josh <jromer94@gmail.com>
2015-01-04 04:46:27 -0500

    added markers to map

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -1,11 +1,22 @@
 var map;
-   function initialize() {
-       var mapOptions = {
-           center: { lat: 40.500, lng: -74.452},
-           zoom:16 
-       };
-   map = new google.maps.Map(document.getElementById('map-canvas'),
+    function initialize() {
+        var mapOptions = {
+            center: { lat: 40.500, lng: -74.452},
+            zoom:16 
+        };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+    for (var crime in crimes) {
+       //change later to for loop
+       crime = crimes[crime]
+       var latLng = new google.maps.LatLng(crime.lat, crime.lng);
+       new google.maps.Marker({
+           position: latLng,
+           map: map,
+           title: crime.descr
+       }); 
+       console.log("marker added");
+    }
    }
 google.maps.event.addDomListener(window, 'load', initialize);
 
