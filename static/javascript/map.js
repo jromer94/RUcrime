1ad300c9cb1a302c57539bb388d257153127a060
Josh <jromer94@gmail.com>
2015-01-05 00:13:52 -0500

    fixed spacing

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -1,40 +1,41 @@
 var map;
-    function initialize() {
-        var mapOptions = {
-            center: { lat: 40.500, lng: -74.452},
-            zoom:16 
-        };
+function initialize() {
+    var mapOptions = {
+        center: { lat: 40.500, lng: -74.452},
+        zoom:16 
+    };
+
     map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
     for (var crime in crimes) {
        //change later to for loop
-       crime = crimes[crime]
+        crime = crimes[crime]
 
-       var contentString =
-       '<h3>' + crime.descr +'</h3>'+
-       '<p>Date: ' + crime.date +
-       '</p>'+
-       '<p> <a href="' + crime.link + '">'+ 'More Info' +
-       '</a></p>'
+        var contentString =
+        '<h3>' + crime.descr +'</h3>'+
+        '<p>Date: ' + crime.date +
+        '</p>'+
+        '<p> <a href="' + crime.link + '">'+ 'More Info' +
+        '</a></p>'
        
-       var latLng = new google.maps.LatLng(crime.lat, crime.lng);
-       var marker = new google.maps.Marker({
-           position: latLng,
-           map: map,
-           title: crime.descr
-       }); 
+        var latLng = new google.maps.LatLng(crime.lat, crime.lng);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            title: crime.descr
+        }); 
        
-       marker.info = new google.maps.InfoWindow({
-           content: contentString
-       });      
+        marker.info = new google.maps.InfoWindow({
+            content: contentString
+        });      
 
 
-       console.log("marker added");
+        console.log("marker added");
     
-       google.maps.event.addListener(marker, 'click', function() {
+        google.maps.event.addListener(marker, 'click', function() {
            this.info.open(map, this);
-       });
+        });
     }
-   }
+}
 google.maps.event.addDomListener(window, 'load', initialize);
 
